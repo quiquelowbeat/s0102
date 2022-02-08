27914d49b90bd4de8bd453b91cb2959ea1716d5f
Quique Sánchez Suárez <kikeebcn@gmail.com>
2022-02-08 21:42:00 +0100

    n1exercici1 - acabado.

---
+++ src/n1exercici1/Main.java
@@ -0,0 +1,38 @@
+package n1exercici1;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		 ArrayList<String> arrayCities = new ArrayList<>();
+		 
+		 arrayCities.add("Barcelona");
+		 arrayCities.add("Lisboa");
+		 arrayCities.add("Sevilla");
+		 arrayCities.add("Manchester");
+		 
+		 PrintWriter out = null;
+		 
+		 try {
+			 out = new PrintWriter(new File ("src/test/Cities.txt")); 
+			 for(String c : arrayCities) {
+				 out.println(c);
+			 }
+		 } catch(IOException e) {
+			 System.err.println("IOException: no se encuentra el directorio");
+		 } finally {
+			 if (out != null) {
+				 System.out.println("Programa finalizado con éxito.");
+				 out.close();
+			 } else {
+				 System.out.println("Saliendo del programa.");
+			 }
+			 
+		 }
+
+	}
+
+}
